fc04cc4feac1989ac7098cffc8c099f5846848e4
DamianT <damian.tapia45@gmail.com>
2023-07-05 19:04:23 -0600

    feat:ost-view on progress

---
+++ React-Challenge25G/src/components/PostView.tsx
@@ -0,0 +1,35 @@
+export default function PostView() {
+  return (
+    <>
+      <main className="flex columns-3">
+        <aside className="flex w-1/3">
+          aside izq donde va la barra de reacciones
+        </aside>
+        <section className="flex w-1/3">
+          <article>
+            <header>
+              <div>
+                <img src="../src/assets/temporal-imgs/unicorn img.webp"></img>
+              </div>
+              <div>
+                <div>
+                  <a>
+                    <img
+                      src="https://randomuser.me/api/portraits/women/60.jpg"
+                      alt="imagen de usuario"
+                      className="rounded-full w-full"
+                    ></img>
+                  </a>
+                </div>
+              </div>
+            </header>
+            <div></div>
+          </article>
+        </section>
+        <aside className="flex w-1/3">
+          aqui va la card del user que creo el post en teoria
+        </aside>
+      </main>
+    </>
+  );
+}
